719f2c48a5d717ff89bb933e63db9c4b51f1ccef
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-03 20:44:11 -0500

    Add Logout Dialog to the Frontend

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,20 @@
 import Sidebar from "./components/sidebar";
 import Feed from "./components/feed";
 import { Toaster } from "./components/ui/toaster";
+import { useStore } from "./lib/store";
 import { LoginDialog } from "./components/login-dialog";
+import { LogoutDialog } from "./components/logout-dialog";
+
 
 function App() {
+  const user = useStore((state) => state.user);
+
   return (
     <div className="flex justify-center min-h-screen gap-3">
       <Sidebar />
       <Feed />
       <div className="flex flex-col gap-2 p-4">
-        <LoginDialog />
+        {user ? <LogoutDialog /> : <LoginDialog />}
       </div>
       <Toaster />
     </div>
