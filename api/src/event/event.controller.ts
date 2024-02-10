d1ce6b74448c2e7901f802a0e68b6124d7ea4ece
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-10 09:50:43 -0500

    Create and Apply Authorization Guard

--- api/src/event/event.controller.ts
+++ api/src/event/event.controller.ts
@@ -5,6 +5,7 @@ import { EventResponseDTO } from './event-response.dto';
 import { UpdateEventDTO } from './update-event.dto';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { UserId } from 'src/decorators/user-id.decorator';
+import { EventOwnershipGuard } from 'src/guards/event-owner.guard';
 
 @Controller('events')
 export class EventController {
@@ -42,36 +43,23 @@ export class EventController {
     return event;
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, EventOwnershipGuard)
   @Patch(':id')
   async update(
     @Param('id') id: string,
     @Body() updateEventDto: UpdateEventDTO,
   ): Promise<EventResponseDTO> {
     const event = await this.eventService.update(id, updateEventDto);
-    if (!event) {
-      throw new NotFoundException(`Event with ID ${id} not found`);
-    }
     delete event.userId;
     return event;
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard, EventOwnershipGuard)
   @Delete(':id')
   async remove(
     @Param('id') id: string,
-    @UserId() userId: number,
   ): Promise<{ statusCode: number; message: string }> {
-    let event = await this.eventService.findOne(id);
-
-    if (!event) {
-      throw new NotFoundException(`Event with ID ${id} not found`);
-    } else if (event.userId !== userId) {
-      throw new ForbiddenException();
-    }
-
-    event = await this.eventService.remove(id);
-
+    await this.eventService.remove(id);
     return {
       statusCode: 200,
       message: 'Event deleted successfully',
---
+++ api/src/guards/event-owner.guard.ts
@@ -0,0 +1,47 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { RequestWithUser } from 'src/decorators/user-id.decorator';
+import { EventService } from 'src/event/event.service';
+
+@Injectable()
+export class EventOwnershipGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private eventService: EventService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    // Get the user id from the request object
+    const user = (request as RequestWithUser).user;
+    const userId = user.userId;
+    // The JWT strategy will throw an error if it fails to validate the token
+
+    // Get the event id from the request params
+    const eventId = request.params.id;
+
+    // If eventId is not provided
+    if (!eventId) {
+      throw new BadRequestException('Invalid or missing event ID');
+    }
+
+    const event = await this.eventService.findOne(eventId);
+
+    // If event does not exist
+    if (!event) {
+      throw new NotFoundException(`event with ID ${eventId} not found`);
+    }
+
+    // Check if the event belongs to the user
+    return event.userId == userId;
+  }
+}
+
