7db9b6f9e243ed5250a14d391419dd9f8f6ab737
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-10 17:14:41 -0500

    Implement global error handling using filters

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { HttpResponseFilter } from "./filters/http-response.filter";
 import { HttpResponseInterceptor } from "./interceptors/http-response.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpResponseFilter());
   app.useGlobalInterceptors(new HttpResponseInterceptor());
   await app.listen(3000);
 }
