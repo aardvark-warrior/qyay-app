a935506661c7801ceb14da6084ec563402c385e3
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-11 20:49:06 -0500

    Add MainView component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,8 @@
-import Sidebar from "./components/sidebar";
-import Feed from "./components/feed";
+// import Sidebar from "./components/sidebar";
+// import Feed from "./components/feed";
+// import Aside from "./components/aside";
+import MainView from "./views/main-view";
 import { Toaster } from "./components/ui/toaster";
-import Aside from "./components/aside";
-// import { LoginDialog } from "./components/auth/login-dialog";
-// import { LogoutDialog } from "./components/auth/logout-dialog";
-// import { RegisterDialog } from "./components/auth/register-dialog";
 import { useStore } from "./lib/store";
 import { useToast } from "./components/ui/use-toast";
 import { useEffect } from "react";
@@ -15,7 +13,6 @@ import {
 } from "./lib/auth";
 
 function App() {
-  // const user = useStore((state) => state.user);
   const clearUser = useStore((state) => state.clearUser);
   const { toast } = useToast();
 
@@ -37,13 +34,10 @@ function App() {
 
   return (
     <div className="flex justify-center min-h-screen gap-3">
-      <Sidebar />
+      {/* <Sidebar />
       <Feed />
-      <Aside />
-      {/* <div className="flex flex-col gap-2 p-4">
-        {user ? <LogoutDialog /> : <LoginDialog />}
-        {!user && <RegisterDialog />}
-      </div> */}
+      <Aside /> */}
+      <MainView />
       <Toaster />
     </div>
   );
