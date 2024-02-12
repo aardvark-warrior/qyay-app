47eedb592a69c6c9fe66a6548246bcd43917253c
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-11 19:05:51 -0500

    Add custom hooks for querying and mutating comments

---
+++ app/src/hooks/use-mutations-questions.tsx
@@ -0,0 +1,28 @@
+import { createQuestion } from "@/lib/api";
+import { useStore } from "@/lib/store";
+import { useToast } from "@/components/ui/use-toast";
+
+function useMutationQuestions() {
+  const { toast } = useToast();
+  const addQuestion = useStore((state) => state.addQuestion);
+  const selectedEventId = useStore((state) => state.selectedEventId);
+
+  const addNewQuestion = async (content: string) => {
+    try {
+      const newQuestion = await createQuestion(selectedEventId as string, content);
+      addQuestion(newQuestion);
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Failed to create the question",
+        description:
+          (error as Error).message ||
+          "There was an error creating the question. Please try again later.",
+      });
+    }
+  };
+
+  return { addNewQuestion };
+}
+
+export default useMutationQuestions;
